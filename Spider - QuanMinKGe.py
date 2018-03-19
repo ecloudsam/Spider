18e352de0c7b8a42e176bfbf1a4bba4d301aa9b1
ecloudsam <36287220+ecloudsam@users.noreply.github.com>
2018-03-19 16:39:31 +0800

    Add files via upload

---
+++ Spider - QuanMinKGe.py
@@ -0,0 +1,12 @@
+from urllib.request import urlopen
+import re
+import urllib
+
+url=input('Enter url: ')
+html=urlopen(url).read().decode('utf-8')
+
+pattern=re.compile(r'http://dl.*m4a')
+get=re.findall(pattern,html)
+
+link=get[0]
+urllib.request.urlretrieve(link, 'C:/Users/ECloud/Downloads/download.m4a')
